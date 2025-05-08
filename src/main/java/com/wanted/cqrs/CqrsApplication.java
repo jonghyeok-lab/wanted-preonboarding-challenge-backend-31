c5a39294ac932a715569e380c212e2b7cda049c6
jonghyeok-97 <dkswhdgur1209@naver.com>
2025-05-08 23:41:36 +0900

    DB 연동

--- src/main/java/com/wanted/cqrs/CqrsApplication.java
+++ src/main/java/com/wanted/cqrs/CqrsApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.wanted.cqrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CqrsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(CqrsApplication.class, args);
     }
 
 }
